3c45290c1d42241fdb0d8e427d40781af823bc51
Thomas Chisholm <thomas.chisholm@outlook.com>
2016-03-16 15:24:35 -0700

    remove distinct

--- controller/search.php
+++ controller/search.php
@@ -19,11 +19,11 @@ class Search extends Database{
 		
 		parent::connect();
 		
-		$searchEventsSQL = "SELECT DISTINCT * FROM `Event` WHERE eventName LIKE ?
+		$searchEventsSQL = "SELECT * FROM `Event` WHERE eventName LIKE ?
 		UNION 
-		SELECT DISTINCT * FROM `Event` WHERE createdBy LIKE ?
+		SELECT * FROM `Event` WHERE createdBy LIKE ?
 		UNION 
-		SELECT DISTINCT * FROM `Event` WHERE description LIKE ?";
+		SELECT * FROM `Event` WHERE description LIKE ?";
 
 		$stmt = $this->conn->prepare($searchEventsSQL);
 		$stmt->bind_param('sss', $searchTarget, $searchTarget, $searchTarget);
