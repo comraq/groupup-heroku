3020f205894c3f1b2f744d771396dbf01ec97d35
Thomas Chisholm <thomas.chisholm@outlook.com>
2016-03-23 19:57:12 -0700

    fixing weird URL thing

--- controller/search.php
+++ controller/search.php
@@ -1,7 +1,9 @@
 <?php
+
 require_once(__DIR__.'/database.php');
 
-class Search extends Database{
+class Search extends Database
+{
 
 	function __construct(){
 		parent::__construct();
@@ -23,105 +25,105 @@ class Search extends Database{
         $searchTarget = "%".$escSearch."%";
 
         $searchEventsSQL = "SELECT 
-    R.eventName AS eventName,
-    R.lat AS lat,
-    R.lon AS lon,
-    R.timeStart AS timeStart,
-    R.timeEnd AS timeEnd,
-    R.cost AS cost,
-    R.description AS description,
-    R.createdBy AS createdBy,
-    R.category AS category,
-    SUM(CASE
+        R.eventName AS eventName,
+        R.lat AS lat,
+        R.lon AS lon,
+        R.timeStart AS timeStart,
+        R.timeEnd AS timeEnd,
+        R.cost AS cost,
+        R.description AS description,
+        R.createdBy AS createdBy,
+        R.category AS category,
+        SUM(CASE
         WHEN uge.email = 'testUser1@test.com' THEN 1
         ELSE 0
-    END) AS going
-FROM
-    (SELECT 
+        END) AS going
+        FROM
+        (SELECT 
         e.eventName AS eventName,
-            e.lat AS lat,
-            e.lon AS lon,
-            e.timeStart AS timeStart,
-            e.timeEnd AS timeEnd,
-            e.cost AS cost,
-            e.description AS description,
-            e.createdBy AS createdBy,
-            GROUP_CONCAT(et.category
-                SEPARATOR ', ') AS category
-    FROM
+        e.lat AS lat,
+        e.lon AS lon,
+        e.timeStart AS timeStart,
+        e.timeEnd AS timeEnd,
+        e.cost AS cost,
+        e.description AS description,
+        e.createdBy AS createdBy,
+        GROUP_CONCAT(et.category
+        SEPARATOR ', ') AS category
+        FROM
         `Event` e
-    NATURAL LEFT JOIN EventTypeHasEvent eht
-    NATURAL LEFT JOIN EventType et
-    WHERE
+        NATURAL LEFT JOIN EventTypeHasEvent eht
+        NATURAL LEFT JOIN EventType et
+        WHERE
         eventName LIKE ?
-            AND NOT EXISTS( SELECT 
+        AND NOT EXISTS( SELECT 
                 *
-            FROM
-                PrivateEvent pe
-            WHERE
-                pe.eventName = e.eventName
-                    AND pe.lat = e.lat
-                    AND pe.lon = e.lon
-                    AND pe.timeStart = e.timeStart
-                    AND pe.timeEnd = e.timeEnd)
-    GROUP BY eventName , lat , lon , timeStart , timeEnd UNION (SELECT 
+        FROM
+        PrivateEvent pe
+        WHERE
+        pe.eventName = e.eventName
+        AND pe.lat = e.lat
+        AND pe.lon = e.lon
+        AND pe.timeStart = e.timeStart
+        AND pe.timeEnd = e.timeEnd)
+        GROUP BY eventName , lat , lon , timeStart , timeEnd UNION (SELECT 
         e.eventName AS eventName,
-            e.lat AS lat,
-            e.lon AS lon,
-            e.timeStart AS timeStart,
-            e.timeEnd AS timeEnd,
-            e.cost AS cost,
-            e.description AS description,
-            e.createdBy AS createdBy,
-            GROUP_CONCAT(et.category
-                SEPARATOR ', ') AS category
-    FROM
+        e.lat AS lat,
+        e.lon AS lon,
+        e.timeStart AS timeStart,
+        e.timeEnd AS timeEnd,
+        e.cost AS cost,
+        e.description AS description,
+        e.createdBy AS createdBy,
+        GROUP_CONCAT(et.category
+        SEPARATOR ', ') AS category
+        FROM
         EventTypeHasEvent eht
-    NATURAL LEFT JOIN `Event` e
-    NATURAL LEFT JOIN EventType et
-    WHERE
+        NATURAL LEFT JOIN `Event` e
+        NATURAL LEFT JOIN EventType et
+        WHERE
         createdBy LIKE ?
-            AND NOT EXISTS( SELECT 
+        AND NOT EXISTS( SELECT 
                 *
-            FROM
-                PrivateEvent pe
-            WHERE
-                pe.eventName = e.eventName
-                    AND pe.lat = e.lat
-                    AND pe.lon = e.lon
-                    AND pe.timeStart = e.timeStart
-                    AND pe.timeEnd = e.timeEnd)
-    GROUP BY eventName , lat , lon , timeStart , timeEnd) UNION (SELECT 
+        FROM
+        PrivateEvent pe
+        WHERE
+        pe.eventName = e.eventName
+        AND pe.lat = e.lat
+        AND pe.lon = e.lon
+        AND pe.timeStart = e.timeStart
+        AND pe.timeEnd = e.timeEnd)
+        GROUP BY eventName , lat , lon , timeStart , timeEnd) UNION (SELECT 
         e.eventName AS eventName,
-            e.lat AS lat,
-            e.lon AS lon,
-            e.timeStart AS timeStart,
-            e.timeEnd AS timeEnd,
-            e.cost AS cost,
-            e.description AS description,
-            e.createdBy AS createdBy,
-            GROUP_CONCAT(et.category
-                SEPARATOR ', ') AS category
-    FROM
+        e.lat AS lat,
+        e.lon AS lon,
+        e.timeStart AS timeStart,
+        e.timeEnd AS timeEnd,
+        e.cost AS cost,
+        e.description AS description,
+        e.createdBy AS createdBy,
+        GROUP_CONCAT(et.category
+        SEPARATOR ', ') AS category
+        FROM
         EventTypeHasEvent eht
-    NATURAL LEFT JOIN `Event` e
-    NATURAL LEFT JOIN EventType et
-    WHERE
+        NATURAL LEFT JOIN `Event` e
+        NATURAL LEFT JOIN EventType et
+        WHERE
         description LIKE ?
-            AND NOT EXISTS( SELECT 
+        AND NOT EXISTS( SELECT 
                 *
-            FROM
-                PrivateEvent pe
-            WHERE
-                pe.eventName = e.eventName
-                    AND pe.lat = e.lat
-                    AND pe.lon = e.lon
-                    AND pe.timeStart = e.timeStart
-                    AND pe.timeEnd = e.timeEnd)
-    GROUP BY eventName , lat , lon , timeStart , timeEnd)) R
+        FROM
+        PrivateEvent pe
+        WHERE
+        pe.eventName = e.eventName
+        AND pe.lat = e.lat
+        AND pe.lon = e.lon
+        AND pe.timeStart = e.timeStart
+        AND pe.timeEnd = e.timeEnd)
+        GROUP BY eventName , lat , lon , timeStart , timeEnd)) R
         NATURAL LEFT JOIN
-    UserGoesEvent uge
-GROUP BY R.eventName , R.lat , R.lon , R.timeStart , R.timeEnd , R.cost , R.description , R.createdBy , R.category";
+        UserGoesEvent uge
+        GROUP BY R.eventName , R.lat , R.lon , R.timeStart , R.timeEnd , R.cost , R.description , R.createdBy , R.category";
 
         $stmt = $this->conn->prepare($searchEventsSQL);
         $stmt->bind_param('sss', $searchTarget, $searchTarget, $searchTarget);
@@ -137,19 +139,18 @@ GROUP BY R.eventName , R.lat , R.lon , R.timeStart , R.timeEnd , R.cost , R.desc
     function startSearchEvents(){
       $reqMethod = $_SERVER['REQUEST_METHOD'];
       if ($reqMethod == 'POST'){
-       $json = file_get_contents("php://input");
-       $data = json_decode($json, TRUE);
-       $result = $this->searchEvents($data);
-       $this->response($result, 200);
-   }else{
-    $result = array(
-        'data' => "Emtpy Data"
-        );
-    $statusCode = 405;
-    $this->response($result, $statusCode);
-    exit;
+         $json = file_get_contents("php://input");
+         $data = json_decode($json, TRUE);
+         $result = $this->searchEvents($data);
+         $this->response($result, 200);
+     }else{
+        $result = array(
+            'data' => "Emtpy Data"
+            );
+        $statusCode = 405;
+        $this->response($result, $statusCode);
+        exit;
+    }
 }
 }
-}
-
 ?>
