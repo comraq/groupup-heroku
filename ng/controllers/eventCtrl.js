44d1a98ad95764faa4d5fb0783c102e3b68b7094
Thomas Chisholm <thomas.chisholm@outlook.com>
2016-03-31 19:41:13 -0700

    fixing bug with search

--- ng/controllers/eventCtrl.js
+++ ng/controllers/eventCtrl.js
@@ -11,7 +11,7 @@ var app = angular.module('groupUpApp').controller('EventCtrl', function($scope, 
     this.deleteEventUrl = "/controller/deleteEvent/startDeleteEvent";
     this.addUserUrl = "/controller/userGoesEvent/startUserGoesEvent";
     this.cancelURL = "/controller/userGoesEvent/startCancelUserGoesEvent";
-    console.log(SessionService.sessionInfo);
+    
     this.detailEvent;
     //search stuff
     this.searchName;
@@ -55,8 +55,11 @@ var app = angular.module('groupUpApp').controller('EventCtrl', function($scope, 
     this.editing = false;
     this.etTabView = false;
     this.eventTypeDetail;
-    this.accountType = SessionService.sessionInfo.accountType;
-
+    this.accountType = -1;
+    this.sessionService = SessionService.sessionInfo;
+    if(this.sessionService){
+        this.accountType = this.sessionService.accountType;
+    }
     var eventTypeToDel;
     var originalEvent;
     var userPosition;
