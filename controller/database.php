df3120dc0c014d06b208db777db156fe3186007f
comraq <yinadam@hotmail.com>
2016-04-02 01:12:38 -0700

    updating database.php

--- controller/database.php
+++ controller/database.php
@@ -2,32 +2,34 @@
 
 require_once(__DIR__.'/rest.inc.php');
 
-class Database extends Rest 
-{
+class Database extends Rest {
 
-	var $servername = "localhost";
-	var $username = "groupup";
-	var $password = "groupup";
-	var $database = "GroupUpDebug";
-	public $conn;
+  var $servername = "db4free.net";
+  var $port = "3306";
+  var $username = "groupup";
+  var $password = "groupup";
+  var $database = "group_up_debug";
+  public $conn;
 		
-	function __construct(){
-		parent::__construct();
-	}
+  function __construct(){
+    parent::__construct();
+  }
 
-	public function connect(){
-		$this->conn = new mysqli($this->servername, $this->username, $this->password);
+  public function connect(){
+    $this->conn = new mysqli($this->servername . ':' . $port,
+                             $this->username,
+                             $this->password);
 
-		if ($this->conn->connect_error){
-			die("Connection failed: " . $this->conn->connect_error."\n");
-		}
-		mysqli_select_db($this->conn, $this->database) or die("Could not connect to " . $this->database);
-	}
+    if ($this->conn->connect_error) {
+      die("Connection failed: " . $this->conn->connect_error."\n");
+    }
+    mysqli_select_db($this->conn, $this->database)
+     or die("Could not connect to " . $this->database);
+  }
 
-	public function disconnect(){
-		$this->conn->close();
-	}
-	
+  public function disconnect(){
+    $this->conn->close();
+  }
 }
 
 
